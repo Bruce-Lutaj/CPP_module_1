24597ce3f906aadc3c3740b6692662585ba5c164
Bruce-Lutaj <lutajbrus@gmail.com>
2024-12-02 18:39:10 +0100

    ex00 completed

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
+#include <cctype> 
 
+int main(int ac, char **av)
+{
+    if (ac < 2)
+    {
+        std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 1;
+    }
+    for (int i = 1; i < ac; i++)
+    {
+        for (int j = 0; av[i] && av[i][j]; j++)
+            av[i][j] = std::toupper(av[i][j]);
+        std::cout << av[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
