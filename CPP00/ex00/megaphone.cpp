fa6ec68e4f54111007d8a7255ef8edd07f36fcaa
Bruce-Lutaj <lutajbrus@gmail.com>
2024-12-02 19:56:24 +0100

    .

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -5,15 +5,14 @@ int main(int ac, char **av)
 {
     if (ac < 2)
     {
-        std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return 1;
     }
     for (int i = 1; i < ac; i++)
     {
-        std::string tmp = av[i];
-        for (size_t j = 0; j < tmp.size(); j++)
-            tmp[j] = std::toupper(static_cast<unsigned char>(tmp[j]));
-        std::cout << tmp << " ";
+        for (size_t j = 0; av[i] && av[i][j]; j++)
+            av[i][j] = std::toupper(static_cast<unsigned char>(av[i][j]));
+        std::cout << av[i] << " ";
     }
     std::cout << std::endl;
     return 0;
